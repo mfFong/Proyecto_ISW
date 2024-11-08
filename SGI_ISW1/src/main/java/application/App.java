08dec89ea6073518205ae0f6e782cbf2f47c75fc
mfFong <135339578+mfFong@users.noreply.github.com>
2024-11-08 11:14:17 -0600

    Add files via upload
    Repositorio del proyecto SGI (Sistema de Gestión de Inventarios).

---
+++ SGI_ISW1/src/main/java/application/App.java
@@ -0,0 +1,14 @@
+package application;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
